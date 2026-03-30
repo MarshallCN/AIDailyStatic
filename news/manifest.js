147d91c1109426d53cb824689ebfec8df6580ec0
MarshallCN <marshallcnliu@gmail.com>
2026-03-31 00:45:28 +0100

    2026-03-31 简报更新

--- news/manifest.js
+++ news/manifest.js
@@ -1,7 +1,8 @@
 window.NEWS_MANIFEST = {
   // Update version after publishing a daily brief.
-  version: '20260329',
+  version: '20260331',
   files: [
+    '2026-03-31.md',
     '2026-03-29.md',
     '2026-03-28.md',
     '2026-03-27.md',
