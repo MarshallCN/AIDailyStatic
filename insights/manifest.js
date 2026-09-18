664c56c6eafd674afd9ad3367f29f5fdbf654102
MarshallCN <marshallcnliu@gmail.com>
2026-09-18 01:19:37 +0100

    Update AI Daily knowledge graph for 2026-09-17

--- insights/manifest.js
+++ insights/manifest.js
@@ -1,6 +1,7 @@
 window.INSIGHT_MANIFEST = {
-  version: '20260917005713',
+  version: '20260918001929',
   files: [
+    '2026-09-17.json',
     '2026-09-16.json',
     '2026-09-15.json',
     '2026-09-14.json',
@@ -192,5 +193,5 @@ window.INSIGHT_MANIFEST = {
     '2026-03-12.json',
     '2026-03-02.json',
   ],
-  latest: '2026-09-16'
+  latest: '2026-09-17'
 };
--- kg/manifest.js
+++ kg/manifest.js
@@ -1,6 +1,7 @@
 window.KG_MANIFEST = {
-  version: '20260917005704',
+  version: '20260918001920',
   files: [
+    '2026-09-17.json',
     '2026-09-16.json',
     '2026-09-15.json',
     '2026-09-14.json',
